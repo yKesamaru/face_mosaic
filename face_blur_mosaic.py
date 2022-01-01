f2ddf87aa1ef8b752dbe92046cd23bd7e7cf4899
yKesamaru <y.kesamaru@tokai-kaoninsho.com>
2022-01-01 16:32:41 +0900

    [add]

--- face_blur_mosaic.py
+++ face_blur_mosaic.py
@@ -2,14 +2,28 @@ import cv2
 import face_recognition
 import PySimpleGUI as sg
 
-vcap = cv2.VideoCapture('img/video.mp4', cv2.CAP_FFMPEG)
-
-
 def mosaic(src, ratio):
     small = cv2.resize(src, None, fx=ratio, fy=ratio,
                        interpolation=cv2.INTER_NEAREST)
     return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
 
+# もし写真にぼかし処理を行いたい場合、コメントアウトを解除する ----------------------
+# image = cv2.imread('img/people.jpg')
+# face_location_list = face_recognition.face_locations(image, 2, 'cnn')
+# if len(face_location_list) > 0:
+#     dst = image.copy()
+#     for (top, right, bottom, left) in face_location_list:
+#         dst[top:top + (bottom - top), left:left + (right - left)] = mosaic(
+#             dst[top:top + (bottom - top), left:left + (right - left)], 0.2)
+#     try:
+#         cv2.imwrite('img/mosaic.jpg', dst)
+#     except:
+#         print('cant write image')
+# else:
+#     print('no face')
+# -----------------------------------------------------------------------------------
+
+vcap = cv2.VideoCapture('img/video.mp4', cv2.CAP_FFMPEG)
 
 sg.theme('Reddit')
 layout = [
